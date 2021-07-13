71d7c11c3a6bc0e6b3255b79a31d5981c3c368f7
wangliang <zymwangliang@163.com>
2021-07-13 18:05:43 +0800

    菜单栏

---
+++ src/components/Home.vue
@@ -0,0 +1,95 @@
+<template>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <span>管理系统</span>
+      </div>
+    </el-header>
+    <!-- 页面主体区 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">
+        <!-- 侧边栏菜单区 -->
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <!-- 一级菜单 -->
+          <el-submenu index="1">
+            <!-- 一级菜单模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i class="el-icon-location"></i>
+              <!-- 文本 -->
+              <span>导航一</span>
+            </template>
+
+            <!-- 二级菜单 -->
+            <el-menu-item index="1-4-1">
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-location"></i>
+                <!-- 文本 -->
+                <span>导航一</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容主体区 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      //左侧菜单数据
+      menulist: [],
+    }
+  },
+  created() {
+    this.getMenuList()
+  },
+  // 获取所有的菜单
+  methods: {
+    async getMenuList() {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menulist = res.data
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  color: #ffffff;
+  font-size: 25px;
+  > div {
+    display: flex;
+    align-items: center;
+  }
+}
+
+.el-aside {
+  background-color: #333744;
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
+</style>
+
+ 
--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+//导入全局样式
+import './assets/css/globle.css'
+
+import axios from 'axios'
+//配置请求根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
